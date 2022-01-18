a8884d8d2f77e9b0ef225a9d5a94fe3c46284f92
ssshazam <ssssshazam@gmail.com>
2022-01-19 02:55:10 +0700

    add sign up test

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe 'Widget management', type: :feature do
-  it 'Log in (test_user@test.com)' do
-    visit sign_in_path
+  it 'Sign up (test_user@test.com)' do
+    visit sign_up_path
     page.fill_in 'Email', with: 'test_user@test.com'
     page.fill_in 'Password', with: '123'
     click_on 'commit'
