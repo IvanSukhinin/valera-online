e678b536391a06f7e2fe87e33b48e2a5d97111d8
ssshazam <ssssshazam@gmail.com>
2022-01-19 16:57:08 +0700

    fix html

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 action_list = [
   ['work', 'Go to work', -5, -30, 0, 70, 100],
   ['nature', 'Enjoy nature', 0, -10, 1, 10, 0],
